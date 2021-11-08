523c5abc583bdc083604501f962ecd30ecbd783f
Federico Longhin <65185337+FedericoLonghin@users.noreply.github.com>
2021-11-08 16:40:45 +0100

    Update markdown css

---
+++ src/routes/scuola/Storia/Prima-guerra-mondiale.svelte
@@ -0,0 +1,38 @@
+<script>
+	import IndicePath from '$lib/IndicePath/index.svelte';
+</script>
+
+<IndicePath />
+<br />
+
+<mh2>Date Importanti</mh2>
+<ul>
+	<li>
+		<mhi>Inizia nel 1918</mhi> (Prima guerra mondiale), che cambierà la storia, finiscono gli imperi
+		e nascono le forme statali
+	</li>
+	<li>
+		<mhi>Finisce nel 1991</mhi> (caduta dell’URSS). Finisce il periodo fascista, le ideologie e il mondo
+		diviso in modello comunista-liberista.
+	</li>
+	<li>
+
+    È compresa anche la caduta del muro di Berlino(<mhi>1989</mhi>). 
+  </li>
+    <li>
+      È detto <mbld>Secolo breve </mbld>(titolo dell'opera di Hobsbawm).
+    </li>
+    <ul>
+
+      <li>
+        <mbld>Titolo rientrato:</mbld> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni necessitatibus aliquid, modi sequi porro non doloremque aut ex in aperiam!
+      </li>
+    </ul>
+
+</ul>
+<coso>
+	<mbld>Lorem ipsum</mbld> dolor sit amet consectetur adipisicing elit. Eius accusamus et laboriosam
+	deserunt repudiandae explicabo voluptate. A neque quasi nesciunt dolore vitae dicta asperiores illum
+	rerum mollitia ipsam at voluptate possimus alias, minus nostrum? Quidem nulla atque numquam, laudantium
+	eligendi expedita corrupti at officia, rerum quod consequuntur dolorem ad quos.
+</coso>
